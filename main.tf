f6b4de140ab47eb5ad91b60c12be6eadd16c635c
cmq008 <50428404+cmq008@users.noreply.github.com>
2023-09-22 17:07:01 -0500

    #13 migrar el estado local de terraform a terraform cloud (#14)

--- main.tf
+++ main.tf
@@ -1,4 +1,19 @@
 terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "PruebasCM"
+
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+  cloud {
+    organization = "PruebasCM"
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
+
   required_providers {
     random = {
       source = "hashicorp/random"
