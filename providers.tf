eab3b60b72ba42e097d4e7545edfaf14b2421de2
cmq008 <cmq008@outlook.com>
2023-09-29 15:49:42 +0000

    #23 el bucket ya no usa random

--- providers.tf
+++ providers.tf
@@ -15,20 +15,11 @@ terraform {
   }
 
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
     aws = {
       source = "hashicorp/aws"
       version = "5.16.2"
     }    
   }
-}
-
-
-provider "random" {
-  # Configuration options
 }
 
 provider "aws" {
